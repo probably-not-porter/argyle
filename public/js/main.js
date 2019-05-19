a8f03ea819b053d912f8bf2d8f3090ecdd20f44d
Porter <pelibby16@earlham.edu>
2019-05-19 18:35:46 -0400

    add settings

--- public/js/main.js
+++ public/js/main.js
@@ -4,17 +4,34 @@ var container = document.getElementById("pattern-container");
 window.onload = function() {
     container = document.getElementById("pattern-container");
     setInterval(main, 20);
+    document.getElementById("colorpick1").value = getComputedStyle(document.documentElement).getPropertyValue('--color-1');
+    document.getElementById("colorpick2").value = getComputedStyle(document.documentElement).getPropertyValue('--color-2');
+    document.getElementById("colorpick3").value = getComputedStyle(document.documentElement).getPropertyValue('--color-3');
+    document.getElementById("colorpick4").value = getComputedStyle(document.documentElement).getPropertyValue('--color-4');
+}
+
+function change1(color){
+    document.documentElement.style.setProperty('--color-1', color);
+}
+function change2(color){
+    document.documentElement.style.setProperty('--color-2', color);
+}
+function change3(color){
+    document.documentElement.style.setProperty('--color-3', color);
+}
+function change4(color){
+    document.documentElement.style.setProperty('--color-4', color);
 }
 
 function main() {
-    if (x > 140){
+    if (x > 560){
         x = 0;
     }
     else{
         x += 2;
     }
-    container.style.marginLeft = (x - 300).toString() + 'px';
-    console.log(x - 300);
+    container.style.marginLeft = (x - 600).toString() + 'px';
+    container.style.marginTop = (x / 2 - 600).toString() + 'px';
 }
 
 
