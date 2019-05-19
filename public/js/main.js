8ffd234a02c72e832714244e73b4d2fae53f388a
probably-not-porter <pelibby16@earlham.edu>
2019-05-19 04:51:45 -0400

    write some stuff

---
+++ public/js/main.js
@@ -0,0 +1,24 @@
+var x = 0;
+var container = document.getElementById("pattern-container");
+
+window.onload = function() 
+{
+    container = document.getElementById("pattern-container");
+    setInterval(main, 20);
+}
+
+function main()
+{
+    if (x > 142)
+    {
+        x = 0;
+    }
+    else
+    {
+        x += 2;
+    }
+    container.style.marginLeft = (x - 300).toString() + 'px';
+    console.log(x - 300);
+}
+
+
